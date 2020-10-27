139170b783741f5f00ca91842c2464809eddba1e
Miles Tucker <milestucker@miless-mbp.lan>
2020-10-27 14:30:41 -0700

    remove js for now

--- src/content.js
+++ src/content.js
@@ -7,8 +7,11 @@ import App from "./App";
 class Main extends React.Component {
     render() {
       return <h1>Hello, Miles</h1>;
-
     }
+}
+
+componentWillMount() {
+  window.test();
 }
 
 const app = document.createElement('div');
@@ -33,4 +36,33 @@ function toggle(){
    }else{
      app.style.display = "none";
    }
+}
+
+function CreateButton() {
+  var dropDownDiv = document.createElement('div');
+  dropDownDiv.className = "dropdown1";
+  dropDownDiv.style.background = "blue";
+  dropDownDiv.style.display = "relative";
+  dropDownDiv.style.position = "absolute";
+  var dropDownDivText = document.createElement('span');
+  dropDownDivText.innerHTML = 'Withizze';
+
+  var dropDownContentDiv = document.createElement('div');
+  dropDownContentDiv.className = "dropdown-content";
+  dropDownContentDiv.style.display = 'none';
+  dropDownContentDiv.style.position = 'absolute';
+  dropDownContentDiv.style.background = '#f9f9f9';
+  dropDownContentDiv.style.minWidth = '160px';
+  dropDownContentDiv.style.boxShadow = '0px 8px 16px 0px rgba(0,0,0,0.2)';
+  dropDownContentDiv.style.padding = '12px 16px';
+  dropDownContentDiv.style.zIndex = '1';
+
+
+  var dropDownContentText= document.createElement('p');
+  dropDownContentText.innerHTML = 'Hello World';
+
+  dropDownContentDiv.appendChild(dropDownContentText)
+  dropDownDiv.appendChild(dropDownDivText)
+  dropDownDiv.appendChild(dropDownContentDiv)
+  return dropDownDiv;
 }
