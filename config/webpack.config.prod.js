2ec69dc26c24207ee62d9f31f36efc3ab3308852
Tyree Stevenson <tyreeostevenson@gmail.com>
2020-10-28 10:24:59 -0400

    Added old code

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -97,7 +97,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -128,7 +128,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -156,7 +156,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               compact: true,
             },
           },
