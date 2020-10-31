1d1ed4649f26d5e6bfdc17cd4b8990ba22f5d937
Tyree Stevenson <tyreeostevenson@gmail.com>
2020-10-31 18:14:44 -0400

    made the top look nice

--- src/RecipeReviewCard.js
+++ src/RecipeReviewCard.js
@@ -25,7 +25,7 @@ import Grid from '@material-ui/core/Grid';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
 import StarIcon from '@material-ui/icons/Star';
 import Divider from '@material-ui/core/Divider'; 
-
+import blueGrey from '@material-ui/core/colors/blueGrey'; 
 const useStyles = makeStyles((theme) => ({
   root: {
     background: 'rgba(255, 255, 255, 0.75)',
@@ -36,7 +36,11 @@ const useStyles = makeStyles((theme) => ({
     left: '0px',
     alignContent: 'center',
   },
-
+  hover: {
+    '&:hover': {
+         backgroundColor: "red"
+       }
+  },
     expand: {
       transform: 'rotate(0deg)',
       marginLeft: 'auto',
@@ -67,6 +71,15 @@ const useStyles = makeStyles((theme) => ({
     verticalAlign: 'bottom',
     height: 18,
     width: 1,
+    backgroundColor: 'black',
+  },
+  dividerTwo: {
+    width: 50,
+    padding: 0,
+    backgroundColor: 'gray',
+  },
+  column: {
+   flexBasis: '33%',
   },
   }));
 
@@ -99,16 +112,25 @@ export default function RecipeReviewCard() {
     <ThemeProvider theme={theme}>
     <Grid container direction="row" justify="center" alignItems="center">
       <Grid item xs>
+        <div className={classes.column} />
+      </Grid>
+      <Grid item xs>
       <Typography>Withizze</Typography>
       </Grid>
       <Grid item xs>
-        <Divider className={classes.icon} style={{ color: "black" }}/>
+        <div className={classes.column} />
       </Grid>
       <Grid item xs>
-        <StarIcon style={{ color: "gold" }} />
+        <Divider className={classes.icon}/>
+      </Grid>
+      <Grid item xs>
+        <StarIcon style={{ color: "gold"}} />
       </Grid>
       <Grid item xs>
         <Typography>4.9</Typography>
+      </Grid>
+      <Grid item xs>
+        <div className={classes.column} />
       </Grid>
       <Grid item xs>
       <AvatarGroup max={4}>
@@ -136,10 +158,10 @@ export default function RecipeReviewCard() {
         </Grid>
         <Collapse in={expanded} timeout="auto" unmountOnExit>
           <CardContent>
-          <Grid container spacing={2} align='center'>
+          <Grid container spacing={2} align="center">
             <Grid item xs={6} align='center'>
               <Avatar align='center' alt="Remy Sharp" src="https://i.ibb.co/tYScJ38/Frame.png"/>
-              <Typography align='center'>Strechy</Typography>
+              <Typography align='center'>Stretchy</Typography>
             </Grid>
             <Grid item xs={6}>
               <Avatar align='center' alt="Travis Howard" src="https://i.ibb.co/tYScJ38/Frame.png" />
