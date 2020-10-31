292bae100744bd17e92b788648e2b7c054292f5a
Miles Tucker <milestucker@Miless-MacBook-Pro.local>
2020-10-31 07:36:11 -0700

    fix card

--- src/RecipeReviewCard.js
+++ src/RecipeReviewCard.js
@@ -21,7 +21,17 @@ import { borders } from '@material-ui/system';
 
 
 const useStyles = makeStyles((theme) => ({
-root: {     background: 'rgba(255, 255, 255, 0.75)',     maxWidth: 300,     borderRadius: '12px',     position: 'absolute',     width: '300px',     left: '0px',     alignContent: 'center',     display: 'flex',     '& > *': {       margin: theme.spacing(1),     }, 
+root: { 
+   background: 'rgba(255, 255, 255, 0.75)', 
+   maxWidth: 300, 
+   borderRadius: '12px', 
+   position: 'relative', 
+   width: '300px', 
+   left: '0px', 
+   alignContent: 'center', 
+   display: 'flex', 
+   '& > *': {       margin: theme.spacing(1),     }, 
+
     expand: {
       transform: 'rotate(0deg)',
       marginLeft: 'auto',
@@ -33,10 +43,17 @@ root: {     background: 'rgba(255, 255, 255, 0.75)',     maxWidth: 300,   
       transform: 'rotate(180deg)',
     },
     buttonIcon:{
-      width: '1px',
-      height: '1px',
+      padding: 0,
+    },
+    avatar:{
+      marginLeft: '-10px',
+    },
+    cardheader:{
+      margin: 0,
+    },
+    cardAction:{
+      padding: 0,
     }
-
   },
 
 }));
@@ -52,9 +69,15 @@ export default function RecipeReviewCard() {
   return (
     <Card className={classes.root}>
       <CardHeader
-        title="Withizze"
+        title="Withizze" style={{margin: 0}}
       />
       <CardActions disableSpacing>
+        <AvatarGroup max={4}>
+          <Avatar alt="Remy Sharp" src="https://i.ibb.co/tYScJ38/Frame.png"  style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}/>
+          <Avatar alt="Travis Howard" src="https://i.ibb.co/tYScJ38/Frame.png" style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}/>
+          <Avatar alt="Cindy Baker" src="https://i.ibb.co/tYScJ38/Frame.png" style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}/>
+          <Avatar alt="Agnes Walker" src="https://i.ibb.co/tYScJ38/Frame.png" style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}/>
+        </AvatarGroup>
         <IconButton
           className={clsx(classes.root.expand, {
             [classes.root.expandOpen]: expanded,
@@ -62,14 +85,7 @@ export default function RecipeReviewCard() {
           onClick={handleExpandClick}
           aria-expanded={expanded}
           aria-label="show more"
-          iconStyle={classes.root.buttonIcon}
         >
-        <AvatarGroup max={4}>
-          <Avatar alt="Remy Sharp" src={chrome.extension.getURL("../public/static/images/miles.jpeg")}  style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}/>
-          <Avatar alt="Travis Howard" src="../public/static/images/miles.jpeg" style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}/>
-          <Avatar alt="Cindy Baker" src="../public/static/images/miles.jpeg" style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}/>
-          <Avatar alt="Agnes Walker" src="../public/static/images/miles.jpeg" style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}/>
-        </AvatarGroup>
           <ExpandMoreIcon />
         </IconButton>
       </CardActions>
